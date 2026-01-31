f9aff089debcd6c91677745120fae8284d6e2e55
jorenbroekema <joren.broekema@gmail.com>
2026-01-31 15:11:28 +0100

    chore: simplify and fix readme snippets

--- examples/advanced/matching-build-files/tokens/index.js
+++ examples/advanced/matching-build-files/tokens/index.js
@@ -1,12 +1,8 @@
 import { readdirSync } from 'node:fs';
-import { dirname } from 'node:path';
-import { fileURLToPath } from 'node:url';
-
-const tokensDir = dirname(fileURLToPath(import.meta.url));
 
 const dirs = (p) =>
   readdirSync(p, { withFileTypes: true })
     .filter((d) => d.isDirectory())
     .map((d) => d.name);
 
-export default dirs(tokensDir);
+export default dirs(import.meta.dirname);
